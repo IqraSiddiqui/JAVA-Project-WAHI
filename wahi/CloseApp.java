1eb836813596ed709f9f1335fdbaa5a148759f67
HaaniaSiddiqui <59687296+HaaniaSiddiqui@users.noreply.github.com>
2021-12-08 22:05:03 -0800

    Add files via upload

--- wahi/CloseApp.java
+++ wahi/CloseApp.java
@@ -15,25 +15,14 @@ public class CloseApp implements Tasks {
     public void performTask(String what,Desktop desk){
         //creating a treemap for each command and it's kkey is the function that is to be executed 
         TreeMap<String, String> tmap= new TreeMap<>();
-        tmap.put("closeChrome","cmd /c start taskkill /im chrome.exe /f") ;
-        tmap.put("closeEdge","cmd /c start taskkill /im MicrosoftEdge.exe /f") ;
-        tmap.put("closeWord","cmd /c start taskkill /im winword.exe /f") ;
-        tmap.put("closeExcel","cmd /c start taskkill /im excel.exe /f") ;
-        tmap.put("closePowerpoint","cmd /c start taskkill /im powerpnt.exe /f") ;
-        tmap.put("closePaint","cmd /c start taskkill /im mspaint.exe /f") ;
-        tmap.put("closeNotepad","cmd /c start taskkill /im notepad.exe /f") ;
-        tmap.put("closeCMD","cmd /c start taskkill /im cmd.exe /f") ;
-        tmap.put("closeCP","cmd /c start taskkill /im control.exe /f") ;
-        tmap.put("closeCal","cmd /c start taskkill /im calculator.exe /f") ;
-        tmap.put("closePlayer","cmd /c start taskkill /im wmplayer.exe /f") ; 
+        tmap=createTMAP();
         Set<Map.Entry<String, String>> entrySet = tmap.entrySet();
         // Convert entrySet to Array using toArray method
         Map.Entry<String, String>[] entryArray = entrySet.toArray(new Map.Entry[entrySet.size()]);
         for (int i=0;i<11;i++){
             if(what.equalsIgnoreCase(entryArray[i].getKey())){
                         work=entryArray[i].getValue();
-                    } 
-
+            } 
         }
         run(work);
     }
@@ -48,5 +37,21 @@ public class CloseApp implements Tasks {
             }
         }
     }
-    //public void createTMAP()
+    
+    //function to create treemap
+    public TreeMap createTMAP(){
+        TreeMap<String, String> tmap= new TreeMap<>();
+        tmap.put("closeChrome","cmd /c start taskkill /im chrome.exe /f") ;
+        tmap.put("closeEdge","cmd /c start taskkill /im MicrosoftEdge.exe /f") ;
+        tmap.put("closeWord","cmd /c start taskkill /im winword.exe /f") ;
+        tmap.put("closeExcel","cmd /c start taskkill /im excel.exe /f") ;
+        tmap.put("closePowerpoint","cmd /c start taskkill /im powerpnt.exe /f") ;
+        tmap.put("closePaint","cmd /c start taskkill /im mspaint.exe /f") ;
+        tmap.put("closeNotepad","cmd /c start taskkill /im notepad.exe /f") ;
+        tmap.put("closeCMD","cmd /c start taskkill /im cmd.exe /f") ;
+        tmap.put("closeCP","cmd /c start taskkill /im control.exe /f") ;
+        tmap.put("closeCal","cmd /c start taskkill /im calculator.exe /f") ;
+        tmap.put("closePlayer","cmd /c start taskkill /im wmplayer.exe /f") ; 
+        return tmap;
+    }
 }
